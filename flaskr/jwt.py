e9a6a97a82da1f06eb2398dd869b1929f1b6fab7
Jeff <442880078@qq.com>
2019-04-25 17:33:54 +0800

    完善tokenFeatrue类，添加身份验证装饰器

--- flaskr/jwt.py
+++ flaskr/jwt.py
@@ -1,23 +1,14 @@
-from flask import(Blueprint,g,jsonify,request)
+from flask import(Blueprint,g,jsonify,request,redirect,url_for)
 from flaskr.createDB import (get_db,close_db)
 import pymysql
 import jwt,time
 from . import myConfig
+from functools import wraps
 
 bp=Blueprint('jwt',__name__)
 
-@bp.route('/getToken',methods=('POST',))
-def token_to_lms():
-    user_id=request.form['user_id']
-    token_content=tokenFeature.get_encoded(tokenFeature,user_id)
-    payload_content=tokenFeature.get_decoded(tokenFeature,token_content)
-    return payload_content
-    #这个函数只是测试一下pyjwt功能，结果不错呀
-    #后面要把这个函数作为与LMS联系的接口，接收LMS传递的userid，返回token
-    #下一步就是做成一个装饰器，在每次请求的时候检查有没有token?
-
 class tokenFeature():
-    #使用staticmethod注解，这个函数就可以在未实例化类的时候调用
+    #使用staticmethod装饰器，这个函数就可以在未实例化类的时候调用
     @staticmethod
     def encode_token(user_id):
         try:
@@ -26,6 +17,7 @@ class tokenFeature():
                 'exp':int(time.time())+86400,
                 'user_id':user_id
             }
+            #time.time()是当前的时间戳，86400是一天的秒数
             qumingzi=jwt.encode(
                 payload,
                 myConfig.SECRET_KEY,
@@ -34,7 +26,8 @@ class tokenFeature():
             return qumingzi
         except Exception as e:
             return e
-    #本方法用于计算token，exp是过期时间，86400是一天的秒数
+    #本方法用于计算token，exp是过期时间，iss为token签发者
+    #Attention!这个qumingzi是b'xxxxxxxxx'这样的，b'和最后的'是多余的
     @staticmethod
     def decode_token(token_to_decode):
         try:
@@ -48,13 +41,46 @@ class tokenFeature():
             return 'token已过期'
         except jwt.InvalidTokenError:
             return '无效token'
+        except Exception as e:
+            return e
     #同理，这个就是解密token获取payload的了...
     def get_encoded(self,user_id):
         return self.encode_token(user_id)
     #这个token内容是一个长长的字符串
     def get_decoded(self,token_to_decode):
-        return jsonify(self.decode_token(token_to_decode))
+        return self.decode_token(token_to_decode)
     #payload是一个字典，所以要转为json后再返回
+    #4.25更新：不转为json了，直接返回字典方便取值
+
+#验证身份的装饰器
+def login_required(func):
+    @wraps(func)
+    #保留源信息，本质是endpoint装饰，否则修改函数名很危险
+    #上面这个对wraps(func)的注释我自己也不懂...
+    def wrapper(*args,**kwargs):
+        token_to_decode=request.headers['Authorization']
+        if not token_to_decode:
+            return redirect('/collector')#此处待修改
+        payload_content=tokenFeature.get_decoded(tokenFeature,token_to_decode)
+        if payload_content['exp'] < int(time.time()):
+            return redirect(url_for('jwt.index'))
+            #登录已过期，需重新登录
+        return func(*args,**kwargs)
+    return wrapper
+
+@bp.route('/getToken',methods=('POST',))
+def token_to_lms():
+    user_id=request.form['user_id']
+    token_content=tokenFeature.get_encoded(tokenFeature,user_id)
+    return token_content
+
+@bp.route('/testToken',methods=('GET','POST'))
+@login_required
+def my_token_test():
+    token_content=request.headers['Authorization']
+    payload_content=tokenFeature.get_decoded(tokenFeature,token_content)
+    return jsonify(payload_content)
+
 
 #题外话。token的内容应该是可以客户端修改的吧，那黑客修改之后我们可以识别出来吗？
 #他只能修改payload部分，emmmm可以，
